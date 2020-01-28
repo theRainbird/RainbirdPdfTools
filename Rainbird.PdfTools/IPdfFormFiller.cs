20e0c805699a6e47b14e0b09f52a3634f3a110b6
Hagen Siegel <hagen.siegel@the-rainbird.de>
2020-01-28 06:13:40 +0100

    V1.0

---
+++ Rainbird.PdfTools/IPdfFormFiller.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Rainbird.PdfTools
+{
+    [ComVisible(true)]
+    [Guid("7F871551-A12A-497B-B822-9EEAFE1A4615")]
+    [InterfaceType(ComInterfaceType.InterfaceIsDual)]
+    public interface IPdfFormFiller
+    {
+        [DispId(1)]
+        string DestinationFilePath { get; }
+
+        [DispId(2)]
+        string SourceFilePath { get; }
+
+        [DispId(3)]
+        void ClosePdfDocument();
+
+        [DispId(4)]
+        string GetFieldValue(string fieldName);
+
+        [DispId(5)]
+        void LoadPdfDocument(string sourcePdfPath, string filledOutPdfPath);
+
+        [DispId(6)]
+        void SetFieldValue(string fieldName, string value);
+
+        [DispId(7)]
+        string[] GetFormFieldNames();
+    }
+}
---
+++ Rainbird.PdfTools/PdfFormFiller.cs
@@ -0,0 +1,83 @@
+﻿
+using iTextSharp.text.pdf;
+using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace Rainbird.PdfTools
+{
+    [ComVisible(true)]
+    [ComDefaultInterface(typeof(IPdfFormFiller))]
+    [ClassInterface(ClassInterfaceType.AutoDispatch)]
+    [Guid("F52CF46C-B59A-44DE-9076-01800D448528")]
+    public class PdfFormFiller : IPdfFormFiller
+    {
+        private PdfStamper _pdfDocument;
+        private FileStream _filledOutStream;
+        private string _sourcePdfPath;
+        private string _filledOutPdfPath;
+
+        public PdfFormFiller()
+        {
+
+        }
+
+        public void LoadPdfDocument(string sourcePdfPath, string filledOutPdfPath)
+        {            
+            ClosePdfDocument();
+
+            _sourcePdfPath = sourcePdfPath;
+            _filledOutPdfPath = filledOutPdfPath;
+
+            _filledOutStream = File.Open(_filledOutPdfPath, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None);
+            
+            _pdfDocument =
+                new PdfStamper(new PdfReader(_sourcePdfPath), _filledOutStream);                     
+        }
+
+        public string GetFieldValue(string fieldName)
+        {
+            return _pdfDocument.AcroFields.GetField(fieldName);
+        }
+
+        public void SetFieldValue(string fieldName, string value)
+        {
+            _pdfDocument.AcroFields.SetField(fieldName, value);
+        }
+
+        public string SourceFilePath
+        {
+            get => _sourcePdfPath;
+        }
+
+        public string DestinationFilePath
+        {
+            get => _filledOutPdfPath;
+        }
+
+        public void ClosePdfDocument()
+        {
+            if (_pdfDocument == null)
+                return;                      
+
+            _sourcePdfPath = null;
+            _filledOutPdfPath = null;
+            
+            _pdfDocument.Close();
+            _pdfDocument = null;
+
+            if (_filledOutStream != null)
+            {
+                _filledOutStream.Close();
+                _filledOutStream = null;
+            }
+        }
+
+        public string[] GetFormFieldNames()
+        {
+
+            return _pdfDocument.AcroFields.Fields.Keys.ToArray();
+        }
+    }
+}
